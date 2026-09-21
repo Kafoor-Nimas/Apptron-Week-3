efb5a9ee7f13d7faee2b4c152760585a39a52cc8
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-09-21 16:57:03 +0530

    Created UserContext hook

---
+++ react-12-context-api/src/context/UserContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState({
+    name: "Kafoor Nimas",
+    id: "EMP-1001",
+    email: "nimas.p@ceylontea.com",
+    department: "Inventory & Logistics",
+    designation: "Senior Inventory Manager",
+    profileImage:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?auto=format&fit=crop&w=300&q=80",
+  });
+
+  return (
+    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
